9bf2ba3b6c3aa339101dc664a9f7ebb07fbfb58f
Hoa Pham <hoaphamxuan2002@gmail.com>
2026-07-02 23:07:31 +0700

    Init: initial project structure with Spring Boot 3.3.0 and Java 17

---
+++ autowash-backend/src/test/java/com/autowash/autowash/backend/AutowashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autowash.autowash.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutowashBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
